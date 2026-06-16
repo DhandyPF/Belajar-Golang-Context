2dc347626d12573d7c9e1a3a7a78ebfa16008ecf
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-06-16 13:08:34 +0700

    Belajar Membuat Context with Value di Golang

--- context_test.go
+++ context_test.go
@@ -13,3 +13,35 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	// A sebagai Parent	
+	contextA := context.Background()
+
+	// B sebagai Parent
+	contextB := context.WithValue(contextA, "b", "B")
+
+	// C sebagai Parent
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+	contextG := context.WithValue(contextC, "g", "G")
+
+	// Memunculkan isi dari Context
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	// Mendapatkan value ari Context
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+	fmt.Println(contextF.Value("d"))
+}
